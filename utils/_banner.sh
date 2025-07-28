9077ef6ab37f82a452cdcb50413024b27c52c694
Emerson Torres <36451877+Apemerson@users.noreply.github.com>
2025-07-28 11:34:55 -0300

    atualizou
    atualizou

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -17,29 +17,45 @@
 #######################################
 print_banner() {
 
-clear
+  clear
+
+  printf "\n\n"
+
+  printf "${GREEN}";
+  printf "                                                     ▄▄█▀▀▀▀▀▀▀█▄▄  \n";
+  printf "                                                   ${GREEN}▄█▀${NC}   ${WHITE}▄▄${NC}      ${GREEN}▀█▄\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}███${NC}         ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}██▄         ${GREEN}█${NC}\n";
+  printf "                                                   ${GREEN}█${NC}     ${WHITE}▀██▄${NC} ${WHITE}██${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}       ${WHITE}▀███▀${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}▀█▄           ▄█▀\n";
+  printf "                                                    ▄█    ▄▄▄▄█▀▀  \n";
+  printf "                                                    █  ▄█▀        \n";
+  printf "                                                    ▀▀▀▀          \n";
+  printf "${NC}";
+
+  printf "\n"
 
 printf "${GREEN}";
-printf "                                                \n";
-printf "███████╗ █████╗ ██████╗  ██████╗██╗  ██╗ █████╗ ████████╗\n";
-printf "╚══███╔╝██╔══██╗██╔══██╗██╔════╝██║  ██║██╔══██╗╚══██╔══╝\n";
-printf "  ███╔╝ ███████║██████╔╝██║     ███████║███████║   ██║   \n";
-printf " ███╔╝  ██╔══██║██╔═══╝ ██║     ██╔══██║██╔══██║   ██║   \n";
-printf "███████╗██║  ██║██║     ╚██████╗██║  ██║██║  ██║   ██║   \n";
-printf "╚══════╝╚═╝  ╚═╝╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   \n";
+
+
+printf ${GREEN}" ★彡[̲V̲̲A̲̲M̲̲O̲̲S̲ ̲C̲̲O̲̲M̲̲E̲Ç̲A̲̲R̲!!!]彡★\n\n";
+printf ${GREEN}"          ░▄░█░░░▄▀▀▀▀▀▄░░░█░▄░\n";
+printf ${GREEN}"          ▄▄▀▄░░░█─▀─▀─█░░░▄▀▄▄\n";
+printf ${GREEN}"          ░░░░▀▄▒▒▒▒▒▒▒▒▒▄▀░░░░\n";
+printf ${GREEN}"          ░░░░░█────▀────█░░░░░\n";
+printf ${GREEN}"          ░░░░░█────▀────█░░░░░\n\n";
+printf ${GREEN}"       ★★★★彡ＴＯＲＲＥＳ彡★★★★\n";
+                                                                                                                                                         
+printf ${GREEN} " \033[1;33m © Torres - https://www.emersontorres.com.br \n";
 printf "${NC}";
 
 
-printf "\n"
-  
-printf "\n"
 
-printf "${YELLOW}";
-printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n";
-printf "              Desenvolvimento: ZapChatPro                \n";
-printf "              Whatsapp: +5527996716613                    \n";
-printf "              E-Mail: zapchatprobr@gmail.com          \n";
-printf "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n";
-printf "${NC}";
-printf "\n"
+  printf "${GREEN}";
+  printf "SISTEMA DE MULTIATENDIMENTO Torres Ticket \n";
+  printf "SUPORTE: +55 43 99655-2155  \n";
+  printf "${NC}";
+
+  printf "\n"
 }
--- variables/_app.sh
+++ variables/_app.sh
@@ -4,16 +4,16 @@
 
 # app variables
 
-jwt_secret=3123123213123
+jwt_secret=1228301212302810
 jwt_refresh_secret=75756756756
 
-deploy_password=ZdGXaUHeZ3EKzU=
+deploy_password=Ap10283012@
 
 #mysql_root_password=$(openssl rand -base64 32)
 
-db_pass=ZdG387FhYsm0olSm097541HMSdS
+db_pass=Ap10283012@
 
 db_user=root
-db_name=whaticket
+db_name=torresticket
 
-deploy_email=deploy@whaticket.com
+deploy_email=botesistemas@gmail.com
